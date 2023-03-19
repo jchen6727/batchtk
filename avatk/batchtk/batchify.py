fde2271cdcaa1aadc3fe4d9be9596fc6af40766b
James <jchen.6727@gmail.com>
2023-03-19 11:33:25 -0400

    more descriptive filenames, batchify update

--- avatk/batchtk/batchify.py
+++ avatk/batchtk/batchify.py
@@ -1,30 +1,37 @@
 import pandas
 import itertools
-def batchify( batch_dict = {}, bin_size = 1, file_label = "batch" ):
+def batchify( batch_dict = {}, bin_size = 1, file_label = None ):
     """
-    batch_dict = {string: generator values}
+    batch_dict = {string: list}
+    bin_size = integer
+    file_label = string
+    --------------------------------------------------------------------------------------------------------------------
+    creates a list of pandas dataframes, if file_label exists, each pandas dataframe is written to a csv file.
     """
-    run = 0
+    bins = []
+
     bin_num = 0
     curr_size = 0
     curr_batch = []
 
-    for batch in dcx(**batch_dict):
+    for run, batch in enumerate(dcx(**batch_dict)):
         batch.update({"run": run})
         curr_batch.append(pandas.Series(batch))
-        run += 1
         curr_size += 1
         if curr_size == bin_size:
-            # write bin to csv
-            filename = "{}{}.csv".format(file_label, bin_num)
-                       
-
-
-
+            bin_df = pandas.DataFrame(curr_batch)
+            curr_batch = []
+            bins.append(bin_df)
+            # write bin to csv if file_label
+            if file_label:
+                filename = "{}{}.csv".format(file_label, bin_num)
+                bin_num += 1
+                bin_df.to_csv(filename, index=False)
+    return bins
 
 def dcx(**kwargs):
     """
-    Dictionary preserving Cartesian (x) product
+    Dictionary preserving Cartesian (x) product, returned as a generator
     https://stackoverflow.com/questions/5228158/cartesian-product-of-a-dictionary-of-lists
     """
     for instance in itertools.product(*kwargs.values()):
