c88e8449d1c5a9557135be9f2e13fe8d6fac992e
James Chen <jchen.6727@gmail.com>
2024-03-18 17:34:48 -0400

    documentation

--- pubtk/netpyne/comm.py
+++ pubtk/netpyne/comm.py
@@ -5,8 +5,8 @@ from neuron import h
 HOST = 0
 
 class Comm(object):
-    def __init__(self, runner_type='socket'):
-        self.runner = specs
+    def __init__(self, runner = specs):
+        self.runner = runner
         h.nrnmpi_init()
         self.pc = h.ParallelContext()
         self.rank = self.pc.id()
