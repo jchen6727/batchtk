52e303272ffb691eeb03c2422096a01fd76d37b9
James Chen <jchen.6727@gmail.com>
2024-05-21 12:27:10 -0400

    trial function, plan to integrate inspyredtk

---
+++ batchtk/inspyredtk/search.py
@@ -0,0 +1,10 @@
+from inspyred import ec
+import random
+
+ALGO = {
+    'DEA': ec.DEA,
+    'EDA': ec.EDA,
+    'ES': ec.ES,
+    'GA': ec.GA,
+    'SA': ec.SA,
+}
--- batchtk/runtk/trial.py
+++ batchtk/runtk/trial.py
@@ -1,12 +1,21 @@
-def trials():
-    pass
+import types
+import pandas
+def trials(configs, label, gen, dispatcher_constructor, project_path, output_path, submit):
+    results = []
+    for tid, config in enumerate(configs):
+        results.append(trial(config, label, tid, dispatcher_constructor, project_path, output_path, submit))
+    return results
 
 
 def trial(config, label, tid, dispatcher_constructor, project_path, output_path, submit):
     run_label = '{}_{}'.format(label, tid)
+    trial.run_label = run_label
+    trial.output_path = output_path
+    for k, v in config.items(): #call any function pointers
+        if isinstance(v, types.FunctionType):
+            config[k] = v()
     dispatcher = dispatcher_constructor(project_path=project_path, output_path=output_path, submit=submit,
                                         gid=run_label)
-
     dispatcher.update_env(dictionary=config)
     try:
         dispatcher.run()
@@ -18,3 +27,9 @@ def trial(config, label, tid, dispatcher_constructor, project_path, output_path,
         raise (e)
     data = pandas.read_json(data, typ='series', dtype=float)
     return data
+
+
+LABEL_POINTER = lambda:trial.run_label
+
+
+PATH_POINTER = lambda:trial.output_path
