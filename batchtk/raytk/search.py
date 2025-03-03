b2bfe9e0e3945b149f8b8b0088528fc80edd4ff4
James Chen <jchen.6727@gmail.com>
2025-03-03 14:49:05 -0600

    TOTP updates to handle BadAuthentication + fixing deprecated calls
    ray.train -> ray.tune
    wrapping string to pandas.read_json
    TOTPConnection which can now be used for SSH w/ MFA

--- batchtk/raytk/search.py
+++ batchtk/raytk/search.py
@@ -1,10 +1,10 @@
 import ray
 import pandas
-from ray import train
+from ray import tune
 import types
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 from batchtk.runtk.trial import trial, LABEL_POINTER, PATH_POINTER
 def ray_trial(config, label, dispatcher_constructor, project_path, output_path, submit, dispatcher_kwargs=None, interval=60):
-    tid = ray.train.get_context().get_trial_id()
+    tid = ray.tune.get_context().get_trial_id()
     tid = tid.split('_')[-1]  # value for trial (can be int/string)
     return trial(config, label, tid, dispatcher_constructor, project_path, output_path, submit, dispatcher_kwargs, interval=interval)
