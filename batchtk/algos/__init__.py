67bfa10892629c7903f949429f0479b499bea4e7
James Chen <jchen.6727@gmail.com>
2025-10-08 08:55:34 -0500

    removed the __init__.py statements from algos
    no one has smac installed, no use case currently

--- batchtk/algos/__init__.py
+++ batchtk/algos/__init__.py
@@ -1,2 +1,6 @@
-from .optuna_utils import *
-from .smac_utils import *
+#from .optuna_utils import *
+#from .smac_utils import *
+
+# manual
+# from batchtk.algos.optuna_utils import optuna_search
+# from batchtk.algos.smac_utils import smac_search
