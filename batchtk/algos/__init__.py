acc8680b1f0dd73cc28d1d230948b217ead93cfd
James Chen <jchen.6727@gmail.com>
2025-10-08 16:29:28 -0500

    make algo imports flexible on dependencies
    missing package creates a dummy ImportError into the module namespace...

--- batchtk/algos/__init__.py
+++ batchtk/algos/__init__.py
@@ -1,6 +1,15 @@
-#from .optuna_utils import *
-#from .smac_utils import *
+import importlib.util
+# check before import 2/2 reliance on non-core dependencies...
 
-# manual
-# from batchtk.algos.optuna_utils import optuna_search
-# from batchtk.algos.smac_utils import smac_search
+def missing_dependency(*args, **kwargs):
+    raise ImportError("This function requires optional package dependencies that are not installed. Please refer to the relevant .py and install them prior to running this function.")
+
+if importlib.util.find_spec('smac'):
+    from .smac_utils import *
+else:
+    smac_search = missing_dependency
+
+if importlib.util.find_spec('optuna'):
+    from .optuna_utils import *
+else:
+    optuna_search = missing_dependency
